0246a2aec6d4318c9336c90a4a10ba6cb4fc8f90
Jaime Freire <jaime.freire@gmail.com>
2016-11-09 15:15:50 +0100

    Comments and removed redundancies

--- src/main/java/client/CommandParser.java
+++ src/main/java/client/CommandParser.java
@@ -13,7 +13,7 @@ import java.util.logging.Logger;
 
 public class CommandParser {
 
-    public static Logger logger = Logger.getLogger("Calculator_Logger");
+    public static final Logger logger = Logger.getLogger("Calculator_Logger");
 
     public static void main(String a[]) {
         new CommandParser().start();
@@ -25,7 +25,6 @@ public class CommandParser {
      */
     public static List<String> infixToPostfixConvert(String input) {
 
-        int priority = 0;
         String postfixBuffer = "";
         Stack<Character> stack = new Stack<Character>();
         List<String> postfixArray = new ArrayList<String>();
@@ -34,44 +33,39 @@ public class CommandParser {
             char ch = input.charAt(i);
             if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
 
+                //If there's already a number in buffer, has to go to the list, put it in the list already.
                 if (postfixBuffer.length() > 0) {
                     postfixArray.add(postfixBuffer);
+                    postfixBuffer = "";
                 }
-                postfixBuffer = "";
+
                 // check the precedence
-                if (stack.size() <= 0)
+                //Empty stack
+                if (stack.size() == 0) {
                     stack.push(ch);
-                else {
+                } else {
+                    //Already a higher prio?
                     Character chTop = stack.peek();
-                    if (chTop == '*' || chTop == '/')
-                        priority = 1;
-                    else
-                        priority = 0;
-                    if (priority == 1) {
-                        if (ch == '+' || ch == '-') {
-                            postfixArray.add(String.valueOf(stack.pop()));
-                            i--;
-                        } else { // Same
-                            postfixArray.add(String.valueOf(stack.pop()));
-                            i--;
-                        }
+                    if (chTop == '*' || chTop == '/') {
+                        //Add higher prio and run again same iteration.
+                        postfixArray.add(String.valueOf(stack.pop()));
+                        i--; //Repeat same loop.
                     } else {
-                        if (ch == '+' || ch == '-') {
-                            postfixArray.add(String.valueOf(stack.pop()));
-                            stack.push(ch);
-                        } else
-                            stack.push(ch);
+                        //Push operator to stack
+                        stack.push(ch);
                     }
                 }
             } else {
+                //Add numbers added to buffer normally
                 postfixBuffer += ch;
             }
         }
+        //Add all operands
         postfixArray.add(postfixBuffer);
-        int len = stack.size();
-        for (int j = 0; j < len; j++)
-            postfixArray.add(stack.pop().toString());
-
+        //All remaining operators added to array at the end and with their prio.
+        while (!stack.isEmpty()) {
+            postfixArray.add("" + stack.pop());
+        }
         return postfixArray;
     }
 
@@ -115,8 +109,6 @@ public class CommandParser {
                 Invoker invoker = new Invoker();
                 invoker.compute(cmd);
                 stack.push(cal.getCurrent());
-            } else if (false) {
-
             } else {
                 stack.push(new BigDecimal(next.trim()));
             }
